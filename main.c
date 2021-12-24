b33aa4d6f3d42c421115354619817e490df66691
Abdelkarim Chedmi <achedmi@e2r12p10.1337.ma>
2021-12-24 17:00:16 +0100

    omicron push

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: achedmi <achedmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 12:12:58 by achedmi           #+#    #+#             */
-/*   Updated: 2021/12/23 14:28:11 by achedmi          ###   ########.fr       */
+/*   Updated: 2021/12/24 16:21:24 by achedmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,143 @@ char	*check_acsess(char *commande)
 	return ("");
 }
 
-void	execute(int i, char **argv, int *fd, int fd0)
-{
-	int		fd1;
-	int		argv_len;
-	int		fk;
-	char	*args[500];
-	char	str[500];
-	int		l = 0;
+// void	execute(int i, char **argv, int *fd, int fd0)
+// {
+// 	int		fd1;
+// 	int		argv_len;
+// 	int		fk;
+// 	char	*args[500];
 
-	argv_len = 0;
-	while (argv[argv_len])
-		argv_len++;
+// 	argv_len = 0;
+// 	while (argv[argv_len])
+// 		argv_len++;
+
+// 	fd1 = open("txt7.txt", O_RDWR | O_TRUNC);
 	
-	fd1 = open("txt7.txt", O_RDWR | O_TRUNC);
+// 	fk = fork();
+// 	if(fk == 0)
+// 	{
+// 		dup2(fd[1], 1);
+// 		close(fd[1]);
+
+// 		args[0] = argv[i];
+// 		args[1] = NULL;		
+// 		execve(check_acsess(argv[i]), args, NULL);
+// 	}	
+// 	wait(NULL);
+// 	i++;
 	
+// 	close(fd0);
+
+// 	fk = fork();
+// 	if(fk == 0)
+// 	{
+		
+// 		dup2(fd[0], 0);
+// 		dup2(fd[1], 1);
+		
+// 		close(fd[0]);
+// 		close(fd[1]);
+		
+// 		args[0] = argv[i];
+// 		args[1] = NULL;				
+// 		execve(check_acsess(argv[i]), args, NULL);
+// 	}	
+// 	wait(NULL);
+// 	i++;
+
+// 	close(fd[1]);
+
+// 	// puts(argv[i]);
 	
+// 	fk = fork();
+// 	if(fk == 0)
+// 	{
+// 		dup2(fd1, 1);
+// 		dup2(fd[0], 0);
+		
+// 		close(fd[0]);
+// 		close(fd1);
+		
+// 		args[0] = argv[i];
+// 		args[1] = NULL;
+		
+// 		execve(check_acsess(argv[i]), args, NULL);
+// 	}	
+// 	wait(NULL);
 	
-	puts(str);
+// 	close(fd1);
+// 	close(fd[0]);
+// 	close(fd[1]);
+// 	i++;
 	
+// }
+
+void	execute(int i, char **argv, int *fd, int argc)
+{
+	int	fd1;
+	int fk;
+	char *args[5];
+
+	// fd1 =  open("txt7.txt", O_RDWR | O_TRUNC);
+	fk = fork();
+	if(fk == 0)
+	{
+		dup2(fd[1], 1);
+		
+		puts("lkjsad");
+		puts("lkjsad");
+		puts("lkjsad");
+		dup2(fd[0], 0);
+		
+		close(fd[0]);
+		close(fd[1]);
+
+		args[0] = argv[i];
+		args[1] = NULL;
+		execve(check_acsess(argv[i]), args, NULL);		
+	}
+	wait(NULL);
+	i++;
+
+	close(fd[0]);
+	close(fd[1]);
+	// fk = fork();
+	// if(fk == 0)
+	// {
+	// 	dup2(fd[0], 0);
+	// 	dup2(fd[1], 1);
+
+	// 	close(fd[0]);
+	// 	close(fd[1]);
+
+	// 	args[0] = argv[i];
+	// 	args[1] = NULL;				
+	// 	execve(check_acsess(argv[i]), args, NULL);		
+	// }
+	// wait(NULL);
+	// i++;
+	
+	/*
+	fk = fork();
+	if(fk == 0)
+	{
+		dup2(fd1, 1);
+		close(fd[0]);
+		close(fd[1]);
+		close(fd1);
+		
+		args[0] = argv[i];
+		args[1] = NULL;				
+		execve(check_acsess(argv[i]), args, NULL);
+	}
+	wait(NULL);
+	i++;
+	*/
+
+	// close(fd[0]);
+	// close(fd[1]);
+	// close(fd1);
 }
 
 int	here_doc(char **argv, int *fd)
@@ -64,7 +182,6 @@ int	here_doc(char **argv, int *fd)
 		free(tmp);
 		tmp = get_next_line(0);
 	}
-	dup2(fd[0], 0);
 	return (3);
 }
 
@@ -73,6 +190,7 @@ int	main(int argc, char **argv, char **env)
 	int		fd[2];
 	int		i;
 	int		fd0;
+	char	*line;
 
 	if ((argc < 5) || (pipe(fd) == -1))
 		return (1);
@@ -81,88 +199,22 @@ int	main(int argc, char **argv, char **env)
 		i = here_doc(argv, fd);
 	else
 	{
-		fd0 = open(argv[i], O_RDONLY);
-		dup2(fd0, 0);
+		fd0 = open(argv[i], O_RDONLY);		
+		while (1)
+		{
+			line = get_next_line(fd0);
+			if(line == NULL)
+				break;
+			write(fd[1], line, ft_strlen(line));
+			free(line);
+		}
+		close(fd0);
 		i = 2;
 	}
-	execute(i, argv, fd, fd0);
+	
+	execute(i, argv, fd, argc);
 	return (0);
 }
-
-
-	// dup2(fd[0], 0);
-	// dup2(fd1, 1);
-	// char *args[] = {commande, NULL};
-	// while (argv[i + 1])
-	// {	
-	// 	if (ft_strlen(check_acsess(argv[i])) != 0)
-	// 	{
-	// 		free(commande);
-	// 		commande = 
-	// 		int fk = fork();
-	// 		if (fk == 0)
-	// 		{
-	// 			execve(commande, args, NULL);
-	// 		}
-	// 		wait(NULL);
-	// 		i++;			
-	// 	}
-	// }
-
-	
-	/*
-	int fk = fork();
-	if (fk == 0)
-	{
-		execve(commande, args, NULL);
-	}*/
-
-// int main()
-// {
-// 	int	fd = open("./txt.txt", O_RDWR);
-// 	char *txt;
-// 	while (ft_strncmp(txt, "hi", 2) != 0)
-// 	{
-// 		txt = get_next_line(0);
-// 	}
-// 	// puts(txt);
-// 	return 0;
-// }
-// int	main(int n, char **args)
-// {
-// 	int	fd = open("./txt.txt", O_RDWR);
-// 	int	fd3 = open("./txt3.txt", O_RDWR | O_TRUNC);
-// 	char *arrs[] = {"/bin/sleep", "5", NULL};
-// 	char *arrs2[] = {"/bin/echo", "kkkkk", NULL};
-// 	char *arrs3[] = {"/bin/echo", "lslslsllslsllsls", NULL};
-
-// 	dup2(fd, 0);
-// 	dup2(fd3, 1);
-
-// 	int fk = fork();
-// 	if (fk == 0)
-// 		execve("/bin/sleep", arrs, NULL);
-// 	else
-// 	{
-// 		wait(NULL);
-// 		int fk2 = fork();
-// 		if (fk2 == 0)
-// 		{
-// 			close(fd3);
-// 			int fd2 = open("./txt2.txt", O_RDWR | O_TRUNC);
-// 			fd3 = open("./txt3.txt", O_RDWR);
-
-// 			dup2(fd3, 0);
-// 			dup2(fd2, 1);
-
-// 			execve("/bin/echo", arrs2, NULL);
-// 		}
-// 		else{
-// 			wait(NULL);
-// 		}
-// 	}
-// 	return 1;
-// }
 
 //infile "ls -l" "wc -l" outfile
 
