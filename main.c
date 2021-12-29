00d60e17d97aa65454a1554f6094160dca79ce3f
scratch <abdekrime.chedmi@gmail.com>
2021-12-29 19:54:38 +0100

    finished, just need to handle cases

--- main.c
+++ main.c
@@ -32,89 +32,87 @@ char	*check_acsess(char *commande)
 	return ("");
 }
 
-void	execute(int i, char **argv, int fds[10][2], int argc)
+void	execute(char **argv, int **fds, int argc)
 {
 	int		fd1;
 	int		fk;
+	int		i;
 
+	i = 0;
 	fd1 = open(argv[argc - 1], O_RDWR | O_TRUNC);
 	while (argv[i + 1])
 	{
 		if (argv[i + 1] == argv[argc - 1])
-			fds[i - 1][1] = fd1;
+			fds[i + 1][1] = fd1;
 		fk = fork();
 		if (fk == 0)
 		{
-			dup2(fds[i - 2][0], 0);
-			dup2(fds[i - 1][1], 1);
-			close(fds[i - 2][0]);
-			close(fds[i - 1][1]);
+			dup2(fds[i][0], 0);
+			dup2(fds[i + 1][1], 1);
+			close(fds[i][0]);
+			close(fds[i + 1][1]);
 			execve(check_acsess(ft_split(argv[i], ' ')[0]),
 				ft_split(argv[i], ' '), NULL);
 		}
 		wait(NULL);
-		close(fds[i - 2][0]);
-		close(fds[i - 1][1]);
+		close(fds[i][0]);
+		close(fds[i + 1][1]);
 		i++;
 	}
 }
 
-int	here_doc(char **argv, int *fd)
+int	here_doc_case(char *limiter, int fd)
 {
 	char	*tmp;
 
 	tmp = get_next_line(0);
-	while (ft_strncmp(tmp, argv[2], ft_strlen(tmp) - 1) != 0)
+	while (ft_strncmp(tmp, limiter, ft_strlen(tmp) - 1) != 0)
 	{
-		write(fd[1], tmp, ft_strlen(tmp));
+		write(fd, tmp, ft_strlen(tmp));
 		free(tmp);
 		tmp = get_next_line(0);
 	}
 	return (3);
 }
 
-int	main(int argc, char **argv, char **env)
+int	file_case(char *file_name, int fd)
 {
-	int		fds[10][2];
-	int		i;
-	int		txt;
 	char	*line;
-	char	*tmp;
+	int		file;
 
-	pipe(fds[0]);
-	pipe(fds[1]);
-	pipe(fds[2]);
-	pipe(fds[3]);
-	pipe(fds[4]);
-	pipe(fds[5]);
-	pipe(fds[6]);
-	if ((argc < 4))
-		return (1);
-	// if ((argc < 4) || (pipe(fd) == -1))
-	// 	return (1);
-	i = 1;
-	if (ft_strncmp(argv[i], "here_doc", 8) == 0)
-		i = here_doc(argv, fds[0]);
-	else
+	file = open(file_name, O_RDONLY);
+	while (1)
 	{
-		txt = open(argv[i], O_RDONLY);
-		while (1)
-		{
-			line = get_next_line(txt);
-			if(line == NULL)
-				break;
-			write(fds[0][1], line, ft_strlen(line));
-		}
-		i = 2;
-		close(txt);
+		line = get_next_line(file);
+		if (line == NULL)
+			break ;
+		write(fd, line, ft_strlen(line));
 	}
-	close(fds[0][1]);
-	execute(i, argv, fds, argc);
-	return (0);
+	close(file);
+	return (2);
 }
 
-// ./a.out txt.txt ls wc txt7.txt
+int	main(int argc, char **argv, char **env)
+{
+	int		**fds;
+	int		i;
+	int		j;
 
-
-//	./pipex file1 cmd1 cmd2 cmd3 ... cmdn file2
-//	 ./pipex here_doc LIMITER cmd cmd1 file
+	j = -1;
+	fds = malloc(10 * sizeof(int *));
+	if (fds == NULL || argc < 5)
+		return (0);
+	while (++j < argc - 2)
+	{
+		fds[j] = malloc(2 * sizeof(int));
+		if (pipe(fds[j]) == -1)
+			return (0);
+	}
+	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+		i = here_doc_case(argv[2], fds[0][1]);
+	else
+		i = file_case(argv[1], fds[0][1]);
+	close(fds[0][1]);
+	execute(&argv[i], fds, argc - i);
+	return (0);
+}
